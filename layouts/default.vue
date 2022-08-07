a5b98cb18da21bb0c59722c7963ffead15790762
minh-ctrl <binhminh140899@gmail.com>
2022-08-07 21:34:11 +0700

    used provide/inject to pass props from layout to pages/index

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,28 @@
 <script setup>
+import { provide } from 'vue';
 const darkMode= ref(false);
 const toggle = () => {
     darkMode.value = !darkMode.value;
 }
+provide('darkMode', darkMode);
 </script>
 <template>
     <div :class="{darkMode: darkMode}" class="default w-screen h-screen" >
         <div class="flex flex-row-reverse">
-            <div class=" 2xl:w-80 lg:w-64 md:w-32 md:fixed md:h-screen
-        text-center flex flex-col md:flex-col overflow-y-auto">
-                    <img :class="{icon_inverted: darkMode}" src="/mokapot.gif"/>
-                <div :class="{darkMode: darkMode}" class="title PressStart2P text-xl mt-10 mb-10 text-slate-200 ">THE_MOKA_POT</div>
-                <Nav :darkMode="darkMode"/>
+            <div class=" l:w-80 lg:w-64 md:w-32 md:fixed md:h-screen text-center flex flex-col md:flex-col overflow-y-auto">
                 <label @change="toggle()" for="green-toggle" class=" self-center relative items-center mr-5 cursor-pointer">
                     <input  type="checkbox" value="" id="green-toggle" class="sr-only peer">
-                    <div class="w-11 h-6 bg-gray-200  peer dark:bg-gray-700 peer-focus:ring-green-300 
+                    <div class="w-11 h-6 bg-white  peer dark:bg-white peer-focus:ring-green-300 
                         dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full 
                         after:content-[''] after:absolute after:top-0.5 after:left-[2px] before:bg-white after:bg-slate-200 after:border-gray-300 
-                        after:border  after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-slate-900"></div>
+                        after:border  after:h-5 after:w-5 after:transition-all peer-checked:bg-slate-900"></div>
                 </label>
+                <img :class="{icon_inverted: darkMode}" src="/mokapot.gif"/>
+                <div :class="{darkMode: darkMode}" class="title PressStart2P text-xl mt-10 mb-10 text-slate-200 ">THE_MOKA_POT</div>
+                <Nav :darkMode="darkMode"/>
             </div>
         </div>
-        <slot/>
+        <slot :darkMode="darkMode">
+        </slot>
     </div>
-
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
+<script setup>
+    import { inject } from 'vue'
+    const darkMode = inject('darkMode');
+</script>
 <template>
-    <div class="text-3xl ">
-       
-    </div>
+        <div :class="{darkMode: darkMode}" class="text-3xl PressStart2P text-white">
+            Testing
+        </div>
 </template>
