bb2c368ad084aa1c7f362df31a76adf936dcba42
minh-ctrl <binhminh140899@gmail.com>
2022-08-13 16:22:42 +0700

    format

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,19 +6,19 @@ const toggle = () => {
     darkMode.value = !darkMode.value;
 };
 watchEffect(()=>{
-useHead({
-  title: 'The Moka Pot',
-  // or, instead:
-  // titleTemplate: (title) => `My App - ${title}`,
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
-  charset: 'utf-8',
-  meta: [
-    { name: 'description', content: 'My amazing site.' }
-  ],
-  bodyAttrs: {
-    class: darkMode.value ? 'darkMode' : 'backgroundDark'
-  }
-});
+  useHead({
+    title: 'The Moka Pot',
+    // or, instead:
+    // titleTemplate: (title) => `My App - ${title}`,
+    viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    charset: 'utf-8',
+    meta: [
+      { name: 'description', content: 'My amazing site.' }
+    ],
+    bodyAttrs: {
+      class: darkMode.value ? 'darkMode' : 'backgroundDark'
+    }
+  });
 });
 </script>
 <template>
